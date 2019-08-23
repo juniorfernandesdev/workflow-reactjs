0805e0cd3f22ad1d3ef6838ed61db254a2751d80
juniorbass8@gmail.com <juniorbass8@gmail.com>
2019-08-23 10:38:10 -0300

    Primeiro Envio

---
+++ src/redux-flow/reducers/todos/todos.test.js
@@ -0,0 +1,8 @@
+import todos from './index'
+import { expect } from 'chai'
+
+
+//checar se o todo é uma funçãosssdfsdfsdf
+it('should todos be a function', () => {
+  expect(todos).to.be.a('function')
+})
