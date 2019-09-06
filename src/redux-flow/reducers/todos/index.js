2c8220a59b34109ac77ecbd8e33c393621c4f3bf
Junior Fernandes <juniorbass8@gmail.com>
2019-09-06 10:34:53 -0300

    Finish Todo List with REDUCER

--- src/redux-flow/reducers/todos/index.js
+++ src/redux-flow/reducers/todos/index.js
@@ -1,28 +1,27 @@
 import { ADD_TODO, TOGGLE_TODO } from './actions'
+import createReducer from '../../reducers/create-reducer'
 
 export const initialState = []
 
-const todos = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return state.concat({
-        id: action.payload.id,
-        text: action.payload.text,
-        resumo: action.payload.resumo,
-        completed: false
-      })
-    case TOGGLE_TODO:
-      return state.map((todo) => {
-        if (todo.id !== action.payload.id) {
-          return todo
-        }
-        return {
-          ...todo,
-          completed: !todo.completed
-        }
-      })
+const todos = createReducer(initialState, {
+  [ADD_TODO]: (state, action) => {
+    return state.concat({
+      id: action.payload.id,
+      text: action.payload.text,
+      resumo: action.payload.resumo,
+      completed: false
+    })
+  },
+  [TOGGLE_TODO]: (state, action) => {
+    return state.map((todo) => {
+      if (todo.id !== action.payload.id) {
+        return todo
+      }
+      return {
+        ...todo,
+        completed: !todo.completed
+      }
+    })
   }
-  return state
-}
-
+})
 export default todos
--- src/redux-flow/reducers/visibility-filter/index.js
+++ src/redux-flow/reducers/visibility-filter/index.js
@@ -1,13 +1,10 @@
 import { SHOW_ALL, SET_VISIBILITY_FILTER } from './actions'
+import createReducer from '../create-reducer'
 
 export const initialState = SHOW_ALL
 
-const visibilityFilter = (state = initialState, action) => {
-    switch(action.type) {
-        case SET_VISIBILITY_FILTER:
-            return action.payload.filter
-    }
-    return state
-}
+const visibilityFilter = createReducer(initialState, {
+    [SET_VISIBILITY_FILTER]: (state,action) => action.payload.filter
+})
 
 export default visibilityFilter
