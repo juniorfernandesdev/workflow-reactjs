79cbe9bd8f32eac09a7368760c11e909ceeb1cfe
Junior Fernandes <juniorbass8@gmail.com>
2019-09-06 10:39:18 -0300

    Comment

--- src/redux-flow/reducers/visibility-filter/index.js
+++ src/redux-flow/reducers/visibility-filter/index.js
@@ -3,8 +3,9 @@ import createReducer from '../create-reducer'
 
 export const initialState = SHOW_ALL
 
+//utulizando o reducer com o create-reducer
 const visibilityFilter = createReducer(initialState, {
     [SET_VISIBILITY_FILTER]: (state,action) => action.payload.filter
-})
+})  
 
 export default visibilityFilter
